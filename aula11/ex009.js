59d30d280b6894b26b72ada681932a9f86eded62
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2023-07-15 00:03:49 -0300

    commit

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,6 @@
-var país = 'Brasil'
-console.log(`Vivendo em ${país}`)
-if (país == 'Brasil') {
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
     console.log('Brasileiro')
 } else {
     console.log('Estrangeiro')
