43a9b86d201f5c5b9f3d7883e8225f7d0ec810b2
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2023-07-17 19:56:45 -0300

    commit

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 14
+console.log(`Vc tem ${idade} anos.`)
+if (idade < 16) {
+    console.log('n vota')
+} else if (idade < 18) {
+    console.log('voto opcional')
+} else {
+    console.log('voto obrigatório')
+}
