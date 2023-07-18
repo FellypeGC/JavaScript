4613314d746b200b3990af45c53e03c2d3504396
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2023-07-18 14:58:28 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,12 +1,17 @@
 var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 10)
+
+
+/*
+var c = 1 
 while (c <= 10) {
     console.log(`Passo ${c}`)
     c++ // c = c + 1
 }
-
-
-
-
+*/
 
 
 
