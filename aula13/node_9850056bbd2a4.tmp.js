1977527cba453b13f3149fff6b1967455e78d8f9
FellypeGC <99624115+FellypeGC@users.noreply.github.com>
2023-07-18 14:44:29 -0300

    commit

---
+++ aula13/node_9850056bbd2a4.tmp.js
@@ -0,0 +1,20 @@
+var c = 1 
+while (c <= 10000000000000000000000) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
